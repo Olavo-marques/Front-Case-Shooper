f27f2a076f6de376937e352129832d91fa84d3c2
Olavo-marques-do-nascimento <omarquesdonascimento@gmail.com>
2022-10-09 23:36:26 -0300

    entrega domingo anoite, incompleto mas até terça finalizo.

--- src/pages/cart/styled-CartPage.js
+++ src/pages/cart/styled-CartPage.js
@@ -2,19 +2,42 @@ import styled from "styled-components"
 
 export const BottonBack = styled.h1`
 cursor: pointer;
+border-radius: 5%;
+background-color: #1DD342;
+padding: 5px;
+`
+export const ContainerBody = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+/* border: 1px solid black; */
+height: 100vh;
 `
 export const Product = styled.div`
 display: flex;
 justify-content: space-between;
-border: 1px solid black;
-width: 45vw;
+/* border: 1px solid black; */
+width: 40vw;
+`
+export const ContainerCartEmpty = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+/* width: 40vw; */
+`
+export const CartImg = styled.img`
+border-radius: 5%;
+width: 50vw;
+height: 50vh;
+
 `
 export const Bottons = styled.div`
 display:flex;
 align-items: center;
 justify-content: space-between;
 width: 35%;
-border: 1px solid red;
+/* border: 1px solid red; */
 `
 export const BottonsMore = styled.div`
 display:flex;
@@ -35,5 +58,14 @@ margin: 0 5px 0 5px;
 border-radius: 50px; 
 &:hover{
     background-color: red;
+}
+`
+export const BottonAddProduct = styled.h1`
+cursor: pointer;
+border-radius: 5%;
+background-color: #1DD342;
+padding: 5px;
+&:hover{
+    background-color: #00FF40;
 }
 `
