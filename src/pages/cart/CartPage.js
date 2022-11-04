6c343abc776910e75c029e45a96608041631a858
Olavo-marques-do-nascimento <omarquesdonascimento@gmail.com>
2022-11-04 16:22:07 -0300

    remoção do console.log

--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -40,13 +40,8 @@ const CartPage = () => {
     addRequest(form)
   }
 
-  console.log("sumCart", sumCart)
-
   const productLess = (indice, productPrice) => {
-
-    console.log("productPrice", productPrice)
     setPrice(price - productPrice)
-    console.log("price", price)
 
     setUpdateCart(true)
 
